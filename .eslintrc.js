decba72a82f3aa0ad5c981b71290ac6de567690f
Vitaly Bykador <vitaly.bykador@yandex.ru>
2023-08-06 23:55:09 +0300

    The project is ready to working.

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true
+    },
+    extends: [
+      'eslint:recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',  
+    },
+    plugins: [
+      '@typescript-eslint'  // Для TypeScript.
+    ],
+    overrides: [ // Используем `overrides`, чтобы ESLint мог проверять файлы как JS, так и TS.
+      {
+        files: ['*.ts', '*.tsx'],
+        extends: [
+          'plugin:@typescript-eslint/recommended', 
+          'plugin:@typescript-eslint/recommended-requiring-type-checking',   
+        ],
+        parserOptions: {
+          tsconfigRootDir: __dirname,  
+          project: ['./tsconfig.json'],  
+        },
+      },
+    ],
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+      quotes: ["error", "double"],  // Использовать двойные кавычки.
+      semi: ["error", "always"],    // Всегда добавлять точку с запятой в конце утверждения.
+      indent: ["error", 4],         // Отступ — это четыре пробела.
+      //"no-console": "error"       // Избегать использования в коде методов на консоли (`console`).
+    }
+  }
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+    <div>
+        <h1>Hello, Welcome to React and TypeScript and JSX and ESLint!!!</h1>
+    </div>
+);
